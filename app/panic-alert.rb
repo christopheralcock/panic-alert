6a66af850e6c001611a7bbf132c565d0bf9df726
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2015-11-22 15:08:56 +0000

    actually got sinatra set up, as little as that achieves

---
+++ app/panic-alert.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+
+class PanicAlert < Sinatra::Base
+
+  get '/' do
+    'Some placeholder text'
+  end
+
+end
---
+++ config.ru
@@ -0,0 +1,4 @@
+require 'rubygems'
+require File.join(File.dirname(__FILE__), 'app/panic-alert.rb')
+
+run PanicAlert
